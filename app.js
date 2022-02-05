be90242ba69a7743f5de3fe0664a36dcf21b71e7
Muhammad Ammar Hussain <94798823+Ammar398@users.noreply.github.com>
2022-02-05 13:02:18 +0500

    work complete
    work complete

---
+++ app.js
@@ -0,0 +1,8 @@
+function details(){
+    var a = document.getElementById("icn");
+    a.style.display = 'block';
+}
+function detail(){
+    var b = document.getElementById("icn1");
+    b.style.display = 'block';
+}
